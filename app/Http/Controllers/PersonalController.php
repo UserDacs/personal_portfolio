164630f7401e0eea5f47d1da9b40d21e82fce2d4
Emil <markcajes40@gmail.com>
2025-02-08 15:28:56 +0800

    fix

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -29,6 +29,42 @@ class PersonalController extends Controller
 
         $files = File::where('user_id',1)->get(); 
 
-        return view('personal-portforlio.index',compact('user_info','user_contact','user_education','user_experience','user_profile','user_skill','id','files'));
+        
+        Log::info("user_info ==================================================");
+        Log::info($user_info);
+        Log::info("==========================================================================================================");
+        Log::info("user_contact ===============================================");
+        Log::info($user_contact);
+        Log::info("==========================================================================================================");
+        Log::info("useruser_education_contact ===============================================");
+        Log::info($user_education);
+        Log::info("==========================================================================================================");
+        Log::info("user_experience ===============================================");
+        Log::info($user_experience);
+        Log::info("==========================================================================================================");
+        Log::info("user_profile ===============================================");
+        Log::info($user_profile);
+        Log::info("==========================================================================================================");
+        Log::info("user_skill ===============================================");
+        Log::info($user_skill);
+        Log::info("==========================================================================================================");
+        Log::info("files ===============================================");
+        Log::info($files);
+        Log::info("==========================================================================================================");
+
+
+        return response()->json([
+            'user_info' => $user_info,
+            'user_contact' => $user_contact,
+            'user_education' => $user_education,
+            'user_experience' => $user_experience,
+            'user_profile' => $user_profile,
+            'user_skill' => $user_skill,
+            'id' => $id,
+            'files' => $files
+        ]);
+
+
+       # return view('personal-portforlio.index',compact('user_info','user_contact','user_education','user_experience','user_profile','user_skill','id','files'));
     }
 }
